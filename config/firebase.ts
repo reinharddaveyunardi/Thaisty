558a7b780fd2ca97142565200a58f479848d17cc
Web File 8 <reinhard.dave08@gmail.com>
2025-04-08 00:43:00 +0700

    Update firebase.ts

--- config/firebase.ts
+++ config/firebase.ts
@@ -3,13 +3,13 @@ import {initializeAuth, getReactNativePersistence, getAuth} from "firebase/auth"
 import {getFirestore} from "@firebase/firestore";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_DOMAIN_AUTH",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID",
-    measurementId: "YOUR_MEASUREMENT_ID",
+    apiKey: "AIzaSyA44tSs_hPTVOi7NnX9dAQHfHZrgAZPhsw",
+    authDomain: "thaisty-bb641.firebaseapp.com",
+    projectId: "thaisty-bb641",
+    storageBucket: "thaisty-bb641.firebasestorage.app",
+    messagingSenderId: "863075536836",
+    appId: "1:863075536836:web:80ded8ae2676d7ed405e91",
+    measurementId: "G-97LV8B4SP8",
 };
 const app = initializeApp(firebaseConfig);
 const firestore = getFirestore(app);
